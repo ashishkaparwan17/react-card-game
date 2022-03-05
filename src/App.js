3bfe54cf2e955d0925587bcd229bbe81f832781e
ashishkaparwan17 <ashishsinghkaparwanask@gmail.com>
2022-03-06 00:32:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import {
+  HashRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Home from './components/Home';
+import Level from './components/Level';
+import { easyLevelImages, mediumLevelImages, hardLevelImages } from './components/Images';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="main-card">
+        <Navbar />
+        <Routes>
+          {/* Since the same <Level> component is being rendered in 3 paths, we have to pass */}
+          {/* a unique key so that they can be distingushed and re-mounted */}
+          <Route path="/" element={<Home />} />
+          <Route path="/easy" element={<Level key="1" images={easyLevelImages} mode="easy"/>} />
+          <Route path="/medium" element={<Level key="2" images={mediumLevelImages} mode="medium"/>} />
+          <Route path="/hard" element={<Level key="3" images={hardLevelImages} mode="hard"/>} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
